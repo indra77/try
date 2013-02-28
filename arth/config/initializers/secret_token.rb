6a58b2cf0b1c18dd93dd6ace49516873762cc1b9
kee <indravadan.jani@misport.com>
2013-02-28 10:07:21 +0530

    rail

---
+++ arth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arth::Application.config.secret_token = '6b44d9acce2c68a0b68dc8d491aa9811bbec5536f27b37211c955be2f9cca4419b19b198deb6e50af235702e72feb3c70732f99a881c79ad82bdc85acae3acaa'
